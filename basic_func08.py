a17b539e13359396fef6537e7cc4c55288c60069
bolbekus <bolbekus@gmail.com>
2023-02-28 19:01:53 +0500

    08

--- basic_func08.py
+++ basic_func08.py
@@ -1,5 +1,9 @@
 # Create a function called main.
-
+def main(a):
+    value = a + 1
+    return value
 # Create function arguments a.
 
 # Increase the value of a to one and return.
+
+print(main(5))
