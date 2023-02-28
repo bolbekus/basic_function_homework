6a3f6735036cf9236cfaff17f61c61100422e0ce
bolbekus <bolbekus@gmail.com>
2023-02-28 19:02:30 +0500

    09

--- basic_func09.py
+++ basic_func09.py
@@ -1,5 +1,12 @@
 # Create a function called main.
+def main(a):
+    value = a - 1
+    return value
+
 
 # Create function arguments a.
 
 # decrease the value of a to one and return
+
+
+print(main(7))
